dacb18e2c72ef88cbf3f933d64e6192ec44bb25b
yosssi <yoshida.keiji.84@gmail.com>
2014-10-09 09:58:16 +0900

    go fmt compile_test.go

--- compile_test.go
+++ compile_test.go
@@ -11,51 +11,51 @@ func TestLineCompile(t *testing.T) {
 	}{
 		{
 			template: `a href=./foo foo`,
-			expect: `<a href="./foo">foo</a>`,
+			expect:   `<a href="./foo">foo</a>`,
 		},
 		{
 			template: `span.color-red red`,
-			expect: `<span class="color-red">red</span>`,
+			expect:   `<span class="color-red">red</span>`,
 		},
 		{
 			template: `span#ref1 text`,
-			expect: `<span id="ref1">text</span>`,
+			expect:   `<span id="ref1">text</span>`,
 		},
 		{
 			template: `span#ref1.color-red.text-big text`,
-			expect: `<span id="ref1" class="color-red text-big">text</span>`,
+			expect:   `<span id="ref1" class="color-red text-big">text</span>`,
 		},
 		{
 			template: `span.color-red#ref1.text-big text`,
-			expect: `<span id="ref1" class="color-red text-big">text</span>`,
+			expect:   `<span id="ref1" class="color-red text-big">text</span>`,
 		},
 		{
 			template: `#ref1 text`,
-			expect: `<div id="ref1">text</div>`,
+			expect:   `<div id="ref1">text</div>`,
 		},
 		{
 			template: `#ref1.color-red.text-big text`,
-			expect: `<div id="ref1" class="color-red text-big">text</div>`,
+			expect:   `<div id="ref1" class="color-red text-big">text</div>`,
 		},
 		{
 			template: `.color-red#ref1.text-big text`,
-			expect: `<div id="ref1" class="color-red text-big">text</div>`,
+			expect:   `<div id="ref1" class="color-red text-big">text</div>`,
 		},
 		{
 			template: "div class=\"dialog {{ if eq .Attr `important` }}color-red{{end}}\" text",
-			expect: "<div class=\"dialog {{if eq .Attr `important`}}color-red{{end}}\">text</div>",
+			expect:   "<div class=\"dialog {{if eq .Attr `important`}}color-red{{end}}\">text</div>",
 		},
 		{
 			template: "div class=\"dialog {{ if eq .Attr `important` }}color-red text-big{{end}}\" text",
-			expect: "<div class=\"dialog {{if eq .Attr `important`}}color-red text-big{{end}}\">text</div>",
+			expect:   "<div class=\"dialog {{if eq .Attr `important`}}color-red text-big{{end}}\">text</div>",
 		},
 		{
 			template: "div class=\"dialog {{ if eq .Attr \"important\" }}color-red{{end}}\" text",
-			expect: "<div class=\"dialog {{if eq .Attr \"important\"}}color-red{{end}}\">text</div>",
+			expect:   "<div class=\"dialog {{if eq .Attr \"important\"}}color-red{{end}}\">text</div>",
 		},
 		{
 			template: "div class=\"dialog {{ if eq .Attr \"important\" }}color-red text-big{{end}}\" text",
-			expect: "<div class=\"dialog {{if eq .Attr \"important\"}}color-red text-big{{end}}\">text</div>",
+			expect:   "<div class=\"dialog {{if eq .Attr \"important\"}}color-red text-big{{end}}\">text</div>",
 		},
 	} {
 		name, filepath := "dummy", "dummy.ace"
